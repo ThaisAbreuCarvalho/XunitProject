2a1261ac9357738ae9b61eb05a2d84fd861f2647
Thais Carvalho <thaisabreucarvalho@gmail.com>
2022-04-19 22:38:27 -0300

    Add project files.

---
+++ XunitTest/Courses/CoursesTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitTest.Courses
+{
+    public class CoursesTest
+    {
+        [Fact(DisplayName ="ShouldCreateCourse")]
+        public void ShouldCreate()
+        {
+
+        }
+    }
+}

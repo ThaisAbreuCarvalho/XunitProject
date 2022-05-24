30d533e2378c6578e65bb868eadaee03e4b68e1c
Thais Carvalho <thaisabreucarvalho@gmail.com>
2022-05-23 23:23:21 -0300

    Initiating tests set up

---
+++ ProjectTests/DomainTests/LoginLogicTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Moq;
+using WebApiDomain;
+using Xunit;
+
+namespace ProjectTests.DomainTests
+{
+    public class LoginLogicTests
+    {
+        private readonly LoginLogic _loginLogic;
+
+        public LoginLogicTests()
+        {
+            _loginLogic = new LoginLogic(new Mock<IMapper>().Object);
+        }
+
+        [Fact(DisplayName = "ShouldNotAcceptInvalidTokenRequest")]
+        public void ShouldNotAcceptInvalidTokenRequest()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "ShouldAcceptValidTokenRequest")]
+        public void ShouldAcceptValidTokenRequest()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "ShouldNotAcceptInvalidUserEmail")]
+        public void ShouldNotAcceptInvalidUserEmail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "ShouldNotAcceptValidUserEmail")]
+        public void ShouldNotAcceptValidUserEmail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "ShouldNotAcceptInvalidUserPassword")]
+        public void ShouldNotAcceptInvalidUserPassword()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "ShouldNotAcceptValidUserPassword")]
+        public void ShouldNotAcceptValidUserPassword()
+        {
+            Assert.True(false);
+        }
+    }
+}
